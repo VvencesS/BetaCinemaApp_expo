e5315e15f67eb7178b47cd0aea0511b10482f2e6
dotrongdung <doibongaea91999@gmail.com>
2021-01-11 00:08:35 +0700

    khuyen ma tin ben le

--- components/khuyenmai/KhuyenMaiMoiComponent.js
+++ components/khuyenmai/KhuyenMaiMoiComponent.js
@@ -1,17 +1,104 @@
 import React, { Component } from 'react';
 import {
-    Text, View, Button,
+    Text, View, Button, StyleSheet, Image
 } from 'react-native';
+import giam10 from '../../images/giam10.jpg';
+import donggia from '../../images/donggia.jpg';
+import uudai from '../../images/uudai.jpg';
+import tk10 from '../../images/tk10.jpg';
+import caiapp from '../../images/caiapp.jpg';
+import sale from '../../images/sale.jpg';
+
+
 
 export default class KhuyenMaiMoiComponent extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-            }}>
-                <Text>Khuyến mại mới</Text>
-                <Button title="Chi tiết khuyến mại" onPress={() => this.props.navigation.navigate('ChiTietKhuyenMai')} />
-            </View>
+            <View style={styles.container}>
+
+                <View style={styles.viewcontainer}>
+
+                    <Image source={giam10} style={styles.image} />
+
+
+                    <Text style={styles.text}>Đặt vé BETA CINEMAS-GIẢM NGAY 10% KHI THANH TOÁN QUA VÍ AIRPAY</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+                    <Image source={donggia} style={styles.image} />
+
+                    <Text style={styles.text}>THÀNH VIÊN BETA - ĐỒNG GIÁ 45K/50K</Text>
+
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={uudai} style={styles.image} />
+
+                    <Text style={styles.text}>BETA TEN</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={tk10} style={styles.image} />
+
+                    <Text style={styles.text}>GIẢM THÊM 10K KHI MUA COMBO ONLINE</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={caiapp} style={styles.image} />
+
+                    <Text style={styles.text}>CÀI APP BETA NHẬN TIỀN BAO LA</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={sale} style={styles.image} />
+
+                    <Text style={styles.text}>SALE KHÔNG NGỪNG- MỪNG "MAD SALE DAY"</Text>
+                </View>
+
+
+            
+
+
+
+
+                <Button style={styles.button} title="Chi tiết khuyến mại" onPress={() => this.props.navigation.navigate('ChiTietKhuyenMai')} />
+            </View >
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        width: null,
+        height: null,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    
+    viewcontainer: {
+        flex: 98,
+        backgroundColor: 'white',
+        marginTop: 10,
+    },
+    button:{
+        flex: 2,
+    },
+    image: {
+        width: 95,
+        height: 70,
+        position: 'absolute',
+        paddingRight: 12
+    },
+
+    text: {
+        fontWeight: 'bold',
+        paddingLeft: 95,
+        paddingBottom: 20,
+    },
+})
--- components/khuyenmai/TinBenLeComponent.js
+++ components/khuyenmai/TinBenLeComponent.js
@@ -1,17 +1,104 @@
 import React, { Component } from 'react';
 import {
-    Text, View, Button,
+    Text, View, Button, StyleSheet, Image
 } from 'react-native';
+import qtrung from '../../images/qtrung.png';
+import ho from '../../images/ho.jpg';
+import GP from '../../images/GP.jpg';
+import thaygiao from '../../images/thaygiao.jpg';
+import us from '../../images/us.jpg';
+import daiuy from '../../images/daiuy.png';
 
-export default class TinBenLeComponent extends Component {
+
+
+export default class KhuyenMaiMoiComponent extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-            }}>
-                <Text>Tin bên lề</Text>
-                <Button title="Chi tiết tin bên lề" onPress={() => this.props.navigation.navigate('ChiTietTinBenLe')} />
-            </View>
+            <View style={styles.container}>
+
+                <View style={styles.viewcontainer}>
+
+                    <Image source={qtrung} style={styles.image} />
+
+
+                    <Text style={styles.text}>Beta Cinemas Quang Trung - Khai Trương đầu năm 2021</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+                    <Image source={ho} style={styles.image} />
+
+                    <Text style={styles.text}>THE CROODS: A NEW AGE| 3 bom tấn hoạt hình không thể bỏ lỡ dịp cuối năm 2020</Text>
+
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={GP} style={styles.image} />
+
+                    <Text style={styles.text}>Beta Cinemas Giải Phóng - Khai Trương 20/11/2020</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={thaygiao} style={styles.image} />
+
+                    <Text style={styles.text}>Anh Thầy Ngôi Sao-Ưowsc Mơ Dang Dở Trở Thành Câu Chuyện Hài Duyên Dáng</Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={us} style={styles.image} />
+
+                    <Text style={styles.text}>Us - Những Kẻ Song Trùng Bí Ẩn Cuồng Loạn </Text>
+                </View>
+
+                <View style={styles.viewcontainer} >
+
+                    <Image source={daiuy} style={styles.image} />
+
+                    <Text style={styles.text}>Đại Úy Marvel - Choáng Ngợp Với Sức Mạnh Không Tưởng Và Cận Chiến Mãn Nhãn</Text>
+                </View>
+
+
+            
+
+
+
+
+                <Button style={styles.button} title="Chi tiết khuyến mại" onPress={() => this.props.navigation.navigate('ChiTietKhuyenMai')} />
+            </View >
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        width: null,
+        height: null,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    
+    viewcontainer: {
+        flex: 98,
+        backgroundColor: 'white',
+        marginTop: 10,
+    },
+    button:{
+        flex: 2,
+    },
+    image: {
+        width: 95,
+        height: 70,
+        position: 'absolute',
+        paddingRight: 12
+    },
+
+    text: {
+        fontWeight: 'bold',
+        paddingLeft: 95,
+        paddingBottom: 20,
+    },
+})
