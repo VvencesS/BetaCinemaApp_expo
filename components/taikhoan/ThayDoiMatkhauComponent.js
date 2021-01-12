585e2426f748a17f109d25e2bb096997aaa4ffb4
khonganh <khonganh.epu@gmail.com>
2021-01-12 12:04:43 +0700

    giao dien thay doi mat khau lam lai

--- components/taikhoan/ThayDoiMatkhauComponent.js
+++ components/taikhoan/ThayDoiMatkhauComponent.js
@@ -5,16 +5,15 @@ import {
   StyleSheet,
   TextInput,
   Dimensions,
-  Alert,
   ScrollView,
   TouchableHighlight,
 } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
-export default class ThayDoiMatKhauComponent extends Component {
-  _onPress = () => {
-    Alert.alert("success");
-  };
-
+export default class DoiMatKhauComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,53 +26,86 @@ export default class ThayDoiMatKhauComponent extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.content}>
-          <Text style={styles.content_one}>Nhập mật khẩu hiện tại</Text>
-          <TextInput
-            style={styles.input}
-            keyboardType="default"
-            placeholder="Mật khẩu hiện tại"
-            secureTextEntry={true}
-            onChangeText={(text) => {
-              this.setState((previousState) => {
-                return {
-                  typePassWord: text,
-                };
-              });
-            }}
-          ></TextInput>
-          <Text style={styles.content_one}>Nhập mật khẩu mới</Text>
-          <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            placeholder="Mật khẩu mới"
-            keyboardType="default"
-            onChangeText={(text) => {
-              this.setState((previousState) => {
-                return {
-                  typeNewPassWord: text,
-                };
-              });
-            }}
-          ></TextInput>
-          <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            placeholder="Nhập lại mật khẩu mới"
-            keyboardType="default"
-            onChangeText={(text) => {
-              this.setState((previousState) => {
-                return {
-                  typeNewPassWordAgain: text,
-                };
-              });
-            }}
-          ></TextInput>
-          <TouchableHighlight>
-            <View style={styles.list_tin}> 
-              <Text style={styles.button}>xác nhận</Text>
+        <ScrollView>
+          <View style={styles.content}>
+            <Text style={styles.content_one}>Nhập mật khẩu hiện tại</Text>
+            <View style={styles.list_item}>
+              <MaterialIcons
+                name="lock-outline"
+                size={40}
+                style={styles.input_icon}
+              />
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Mật khẩu hiện tại"
+                placeholderTextColor="#00000061"
+                underlineColorAndroid="transparent"
+                onChangeText={(text) => {
+                  this.setState((previousState) => {
+                    return {
+                      typePassWord: text,
+                    };
+                  });
+                }}
+              />
             </View>
-          </TouchableHighlight>
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.content_one}>Nhập mật khẩu mới</Text>
+            <View style={styles.list_item}>
+              <MaterialCommunityIcons
+                name="lock-open-check"
+                size={40}
+                style={styles.input_icon}
+              />
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Mật khẩu mới"
+                placeholderTextColor="#00000061"
+                keyboardType="default"
+                underlineColorAndroid="transparent"
+                onChangeText={(text) => {
+                  this.setState((previousState) => {
+                    return {
+                      typeNewPassWord: text,
+                    };
+                  });
+                }}
+              />
+            </View>
+            <View style={styles.list_item}>
+              <MaterialCommunityIcons
+                name="lock-check"
+                size={40}
+                style={styles.input_icon}
+              />
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Nhập lại mật khẩu mới"
+                keyboardType="default"
+                placeholderTextColor="#00000061"
+                underlineColorAndroid="transparent"
+                onChangeText={(text) => {
+                  this.setState((previousState) => {
+                    return {
+                      typeNewPassWordAgain: text,
+                    };
+                  });
+                }}
+              />
+            </View>
+          </View>
+
+          <View style={styles.list_tin}>
+            <TouchableHighlight activeOpacity={0.5} underlayColor="#DDDDDD">
+              <View>
+                <Text style={styles.button}>cập nhật</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
         </ScrollView>
       </View>
     );
@@ -84,45 +116,52 @@ const { width, height } = Dimensions.get("screen");
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
-    marginTop: 16,
-    width: "100%",
-    height: "100%",
+    width: width,
+    height: height,
     backgroundColor: "#e6eef2",
+    alignItems: "center",
   },
   content: {
     flexDirection: "column",
   },
   content_one: {
-    color: "#74c8f2",
-    paddingStart: 15,
-    fontSize: 20,
-    paddingTop: 25,
+    color: "#403b3b",
+    fontSize: 18,
+    paddingTop: 20,
+    fontWeight: "500",
   },
   list_tin: {
     flexDirection: "column",
-    flex: 6,
+    alignSelf: "center",
   },
   list_item: {
     flexDirection: "row",
+    marginTop: 15,
   },
   input: {
-    borderColor: "black",
+    borderColor: "gray",
     borderWidth: 1,
-    width: width / 1.3,
-    padding: 10,
-    marginTop: 10,
-    marginStart: 15,
-    fontSize: 16,
+    width: width / 1.2,
+    fontSize: 18,
+    height: height / 14,
+    borderRadius: 5,
+    paddingLeft: 50,
+  },
+  input_icon: {
+    position: "absolute",
+    color: "gray",
+    alignSelf: "center",
+    borderRightWidth: 4,
   },
   button: {
-    alignSelf: "center",
+    textAlign: "center",
     padding: 15,
     marginTop: 25,
     fontSize: 25,
     textTransform: "uppercase",
     color: "white",
     backgroundColor: "#db7623",
-    width: width / 1.3,
-    borderRadius: 6,
+    width: width / 1.2,
+    height: height / 12,
   },
 });
